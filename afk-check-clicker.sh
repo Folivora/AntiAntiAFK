23ad1baa39bd6ec0734b1bf4417c35496ba4e3a2
Folivora <a.folivora.delsuc@gmail.com>
2023-01-26 15:09:40 +0300

    Rewrited hight coordinate detection for optimization.

--- afk-check-clicker.sh
+++ afk-check-clicker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 TmpScrFile="/tmp/test-ocr.png"
-TmpBWScrFile="/tmp/test-ocr-bw.png"
+TmpBWScrFile="tmp/test-ocr-bw.png"
 TmpTxtOcrFile="/tmp/test-ocr.txt"  # remove later
 LogDir="logs/"
 LogFile=$LogDir"afk_clicker.log"
@@ -67,7 +66,6 @@ do
         if [ `tesseract -l eng $currScrFile_rightside - quiet | grep "$TriggerPhrase" | wc -l` -ge 1 ]; then found_rightside=1; fi
 
       fi
-
   
       if   [ $found_leftside -eq "1" ] && [ $found_rightside -eq "0" ]; then
         currScrFile=$currScrFile_leftside
@@ -99,7 +97,6 @@ do
           done
         fi
 
-
         # Iteratively crop the right part of currScrFile utill trigger phrase will cease to be recognizable by OCR. 
         currPixOffset=0; step=10
         while true
@@ -108,7 +105,7 @@ do
           currWidth=`identify -format '%w' $currScrFile`
 
           convert $currScrFile +repage -crop\
-                  `expr $currWidth - $step`"x"$currHight"+"`expr $step`"+0"   $currScrFile"O"
+                  `expr $currWidth - $step`"x"$currHight"+"$step"+0"   $currScrFile"O"
 
           if [ `tesseract -l eng $currScrFile"O" - quiet 2>&1 | grep "$TriggerPhrase" | wc -l` -eq 0 ]; then 
             # The trigger phrase cant be recognize now.
@@ -124,32 +121,36 @@ do
     done
     logger "coordinateWidth is $coordinateWidth"
   
-  
+
     ################################################################
     ## Trying to find coordinates of the trigger phrase BY HIGHT. ##
     ################################################################
-    
+
     coordinateHight=0 # The TriggerPhrase is located below this coordinate.
   
     currWidth=`identify -format '%w' $currScrFile`
     CoordinateFound=false
+
     until $CoordinateFound
     do
       # Split current part of screenshot into two
       currHight=`identify -format '%h' $currScrFile`
       newHight=`expr $currHight / 2`
+
+      # Cut off the top half and search trigger phrase
+      found_topside=0; found_bottomside=0
       currScrFile_topside=$currScrFile"t"
-      currScrFile_bottomside=$currScrFile"b"
-      convert $currScrFile +repage -crop `echo $currWidth"x"$newHight"+0+0"`          $currScrFile_topside
-      convert $currScrFile +repage -crop `echo $currWidth"x"$newHight"+0+"$newHight` $currScrFile_bottomside
-      #echo $currScrFile  $currWidth $currHight $newHight
-  
-      # Search trigger phrase into both pieces
-      found_topside=0
-      found_bottomside=0
-      if [ `tesseract -l eng $currScrFile_topside  - quiet | grep "$TriggerPhrase" | wc -l` -ge 1 ]; then found_topside=1; fi
-      if [ `tesseract -l eng $currScrFile_bottomside - quiet | grep "$TriggerPhrase" | wc -l` -ge 1 ]; then found_bottomside=1; fi
-  
+      convert $currScrFile +repage -crop $currWidth"x"$newHight"+0+0"          $currScrFile_topside
+      if [ `tesseract -l eng $currScrFile_topside  - quiet | grep "$TriggerPhrase" | wc -l` -ge 1 ]; then found_topside=1; 
+
+      else # If phrase not found in the top half
+        # Cut off the bottom half and check trigger phrase here.
+        currScrFile_bottomside=$currScrFile"b"
+        convert $currScrFile +repage -crop $currWidth"x"$newHight"+0+"$newHight $currScrFile_bottomside
+        if [ `tesseract -l eng $currScrFile_bottomside - quiet | grep "$TriggerPhrase" | wc -l` -ge 1 ]; then found_bottomside=1; fi
+
+      fi
+
       if   [ $found_topside -eq "1" ] && [ $found_bottomside -eq "0" ]; then
         currScrFile=$currScrFile_topside
   
@@ -158,22 +159,44 @@ do
         currScrFile=$currScrFile_bottomside
   
       elif [ $found_topside -eq "0" ] && [ $found_bottomside -eq "0" ]; then
-        # The trigger phrase was splited and cannot be found.
-        # Crops the previus succesfull image on the right side by pixel offset utill trigger phrase will reached. 
-        i=0
-        PixOffset=10 # Pixel offset.  
+        # The trigger phrase was splited and cannot be found by OCR.
+        # Crops the previus succesfull part of screenshot to the top of the middle. 
+        currPixOffset=0; step=35
+        if [ `expr $newHight + $step` -lt $currHight  ]; then 
+          while true
+          do 
+            currPixOffset=`expr $currPixOffset + $step`
+
+            convert $currScrFile +repage -crop\
+                    $currWidth"x"`expr $newHight + $currPixOffset`"+0+"`expr $newHight - $currPixOffset`  $currScrFile"o"
+
+
+            if [ `tesseract -l eng $currScrFile"o"  - quiet 2>&1 | grep "$TriggerPhrase" | wc -l` -eq 1 ]; then 
+              # The trigger phrase can be recognized by OCR now.
+              currScrFile=$currScrFile"o"
+              coordinateHight=`expr $coordinateHight + $newHight - $currPixOffset`
+              break
+            fi
+          done
+        fi
+
+        # Iteratively crop the top part of currScrFile utill trigger phrase will cease to be recognizable by OCR. 
+        currPixOffset=0; step=8
         while true
         do
-          i=`expr $i + $PixOffset` 
+          currPixOffset=`expr $currPixOffset + $step`
           currHight=`identify -format '%h' $currScrFile`
-          newHight=`expr $currHight - $i`
-          convert $currScrFile +repage -crop `echo $currWidth"x"$newHight"+0+"$i` $currScrFile"O"
-          if [ `tesseract -l eng $currScrFile"O"  - quiet 2>&1 | grep "$TriggerPhrase" | wc -l` -eq 0 ]; then 
-            # The trigger phrase was reached.
-            coordinateHight=`expr $coordinateHight + $i - $PixOffset`
+
+          convert $currScrFile +repage -crop\
+                  $currWidth"x"`expr $currHight - $step`"+0+"$step   $currScrFile"O"
+
+          if [ `tesseract -l eng $currScrFile"O" - quiet 2>&1 | grep "$TriggerPhrase" | wc -l` -eq 0 ]; then 
+            # The trigger phrase cant be recognize now.
+            coordinateHight=`expr $coordinateHight + $currPixOffset - $step`
             CoordinateFound=true
             break
           fi
+          currScrFile=$currScrFile"O"
         done
   
       fi 
